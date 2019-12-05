4fa3909b1857ebb8295b240039b121162da962e0
Kronykus <MoreheadSteve@gmail.com>
2019-12-05 07:16:40 -0600

    validatePost and formatting

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,39 +3,39 @@ const express = require('express');
 const router = express.Router();
 
 const usersDb = require( '../users/userDb' );
-const postsDb = require( '../posts/postDb' );
+const postsDb = require( './postDb'        );
 
 router.use( express.json() );
 
 
-router.get('/', (req, res) => {
+router.get( '/', (req, res) => {
   postsDb.get( req.query )
     .then( posts => {
       res.status( 200 ).json( posts );
-    })
+    } )
     .catch( error => {
       console.log( error );
       res.status( 500 ).json( { error: 'there was a problem getting posts' } )
     } );
 });
 
-router.get('/:id', validatePostId, (req, res) => {
+router.get( '/:id', validatePostId, (req, res) => {
   if ( req.post ){ res.status( 200 ).json( req.post ) }
   else { res.status( 400 ).json( { message: "invalid post" } ) }
 });
 
-router.delete('/:id', validatePostId, (req, res) => {
+router.delete( '/:id', validatePostId, (req, res) => {
   postsDb.remove( req.params.id )
     .then( response => {
       res.status( 200 ).json( { message: `${response} record(s) deleted` } );
-    })
+    } )
     .catch( error => {
       console.log( error );
       res.status( 500 ).json( { error: "there was a problem removing the post" } )
     } );
 });
 
-router.put('/:id', validatePostId, validatePost, (req, res) => {
+router.put( '/:id', validatePostId, validatePost, (req, res) => {
   postsDb.update( req.params.id, { text: req.body.text } )
   .then( post => {
     res.status( 200 ).json( post );
@@ -48,7 +48,7 @@ router.put('/:id', validatePostId, validatePost, (req, res) => {
 
 // custom middleware
 
-function validatePostId(req, res, next) {
+function validatePostId( req, res, next ) {
   postsDb.getById( req.params.id )
   .then( post => {
     if ( post ) { req.post = post; next(); }
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const usersDb = require( '../users/userDb' );
+const usersDb = require( './userDb'        );
 const postsDb = require( '../posts/postDb' );
 
 router.use( express.json() );
 
-router.post('/', validateUser, ( req, res ) => {
+router.post( '/', validateUser, ( req, res ) => {
   usersDb.insert( req.body )
     .then( user => {
       res.status( 200 ).json( user );
@@ -18,7 +18,7 @@ router.post('/', validateUser, ( req, res ) => {
     } );
 } );
 
-router.post('/:id/posts', validateUserId, validatePost, ( req, res ) => {
+router.post( '/:id/posts', validateUserId, validatePost, ( req, res ) => {
   postsDb.insert( { user_id: req.params.id, text: req.body.text } )
     .then( post => {
       res.status( 200 ).json( post );
@@ -29,11 +29,11 @@ router.post('/:id/posts', validateUserId, validatePost, ( req, res ) => {
     } );
 } );
 
-router.get('/', ( req, res ) => {
+router.get( '/', ( req, res ) => {
   usersDb.get( req.query )
     .then( users => {
       res.status( 200 ).json( users );
-    })
+    } )
     .catch( error => {
       console.log( error );
       res.status( 500 ).json( { error: 'there was a problem getting users' } )
@@ -45,11 +45,11 @@ router.get('/:id', validateUserId, ( req, res ) => {
   else { res.status( 400 ).json( { message: "invalid user" } ) }
 });
 
-router.get('/:id/posts', validateUserId, ( req, res ) => {
+router.get( '/:id/posts', validateUserId, validatePost, ( req, res ) => {
   usersDb.getUserPosts( req.params.id )
     .then( posts => {
       res.status( 200 ).json( posts );
-    })
+    } )
     .catch( error => {
       console.log( error );
       res.status( 500 ).json( { error: "there was a problem getting user's posts" } )
@@ -60,7 +60,7 @@ router.delete('/:id', validateUserId, ( req, res ) => {
   usersDb.remove( req.params.id )
     .then( response => {
       res.status( 200 ).json( { message: `${response} record(s) deleted` } );
-    })
+    } )
     .catch( error => {
       console.log( error );
       res.status( 500 ).json( { error: "there was a problem removing the user" } )
@@ -91,7 +91,7 @@ function validateUserId( req, res, next ) {
 function validateUser( req, res, next ) {
   const userData = req.body;
 
-  if (userData) {
+  if ( userData ) {
     if ( userData.name ) { next(); }
     else { res.status( 400 ).json( { message: "missing required name field" } ) }
   } else { res.status( 400 ).json( { message: "missing user data" } ) }
